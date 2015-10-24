0a9c75438fde682818baafbf8294dd5f7bbce9c0
Hasan <hasan.akniyev@gmail.com>
2015-10-25 00:00:49 +0300

    Ugly fix for smart.filter

--- bitrix/templates/webfly_san/components/bitrix/catalog/santech1_5/bitrix/catalog.smart.filter/santech/ajax.php
+++ bitrix/templates/webfly_san/components/bitrix/catalog/santech1_5/bitrix/catalog.smart.filter/santech/ajax.php
@@ -1,10 +1,9 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <?
 
-define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/log.txt");
-addMessage2Log($arResult);
-echo "<h2>TEST</h2>";
-//$APPLICATION->RestartBuffer();
+//define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/log.txt");
+//addMessage2Log($arResult);
+$APPLICATION->RestartBuffer();
 echo "<!--JSON-->";
 unset($arResult["COMBO"]);
 echo str_replace("'",'"',CUtil::PHPToJSObject($arResult, true));
