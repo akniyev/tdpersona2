7e53e7c482a6aebad5b4638a2e3c22fa8920aad5
de4dp007 <x@x.x>
2015-11-04 01:52:50 +0300

    brands fixed, gallery started

--- about/gallery/index.php
+++ about/gallery/index.php
@@ -2,55 +2,97 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Галерея");
 ?><div class="wrapper">
-	<div class="container">
+	<div class="container" style="width:100%;">
 		<div class="container-hold">
-			<h1>Галерея работ</h1>
-			 <?$APPLICATION->IncludeComponent(
-	"bitrix:photogallery",
-	"",
-	Array(
-		"ADDITIONAL_SIGHTS" => array(""),
-		"ALBUM_PHOTO_SIZE" => "120",
-		"CACHE_TIME" => "3600",
-		"CACHE_TYPE" => "A",
-		"COMPONENT_TEMPLATE" => ".default",
-		"DATE_TIME_FORMAT_DETAIL" => "d.m.Y",
-		"DATE_TIME_FORMAT_SECTION" => "d.m.Y",
-		"DRAG_SORT" => "Y",
-		"ELEMENTS_PAGE_ELEMENTS" => "50",
-		"ELEMENT_SORT_FIELD" => "sort",
-		"ELEMENT_SORT_ORDER" => "desc",
-		"IBLOCK_ID" => "14",
-		"IBLOCK_TYPE" => "news",
-		"JPEG_QUALITY" => "100",
-		"JPEG_QUALITY1" => "100",
-		"ORIGINAL_SIZE" => "1280",
-		"PAGE_NAVIGATION_TEMPLATE" => "",
-		"PATH_TO_FONT" => "default.ttf",
-		"PATH_TO_USER" => "",
-		"PHOTO_LIST_MODE" => "Y",
-		"SECTION_PAGE_ELEMENTS" => "15",
-		"SECTION_SORT_BY" => "UF_DATE",
-		"SECTION_SORT_ORD" => "DESC",
-		"SEF_FOLDER" => "/about/gallery/",
-		"SEF_MODE" => "Y",
-		"SEF_URL_TEMPLATES" => Array("detail"=>"#SECTION_ID#/#ELEMENT_ID#/","detail_edit"=>"#SECTION_ID#/#ELEMENT_ID#/action/#ACTION#/","detail_list"=>"list/","index"=>"index.php","search"=>"search/","section"=>"#SECTION_ID#/","section_edit"=>"#SECTION_ID#/action/#ACTION#/","section_edit_icon"=>"#SECTION_ID#/icon/action/#ACTION#/","upload"=>"#SECTION_ID#/action/upload/"),
-		"SET_TITLE" => "Y",
-		"SHOWN_ITEMS_COUNT" => "6",
-		"SHOW_LINK_ON_MAIN_PAGE" => array("id","shows","rating","comments"),
-		"SHOW_NAVIGATION" => "N",
-		"SHOW_TAGS" => "N",
-		"THUMBNAIL_SIZE" => "100",
-		"UPLOAD_MAX_FILE_SIZE" => "64",
-		"USE_COMMENTS" => "N",
-		"USE_LIGHT_VIEW" => "Y",
-		"USE_RATING" => "N",
-		"USE_WATERMARK" => "Y",
-		"WATERMARK_MIN_PICTURE_SIZE" => "800",
-		"WATERMARK_RULES" => "USER"
-	)
+			<h1 style="padding:29px 0px 29px 36px;margin-bottom: 0px;"> <? $APPLICATION->ShowTitle(); ?> </h1>
+			 <?$APPLICATION->IncludeComponent("bitrix:news", "gallery", Array(
+	"ADD_ELEMENT_CHAIN" => "Y",	// Включать название элемента в цепочку навигации
+		"ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+		"COLOR_NEW" => "3E74E6",
+		"COLOR_OLD" => "C0C0C0",
+		"COMPONENT_TEMPLATE" => "stafflist",
+		"DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+		"DETAIL_DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+		"DETAIL_DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"DETAIL_FIELD_CODE" => "",	// Поля
+		"DETAIL_PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"DETAIL_PAGER_TEMPLATE" => "",	// Название шаблона
+		"DETAIL_PAGER_TITLE" => "Страница",	// Название категорий
+		"DETAIL_PROPERTY_CODE" => "",	// Свойства
+		"DETAIL_SET_CANONICAL_URL" => "N",	// Устанавливать канонический URL
+		"DISPLAY_AS_RATING" => "rating",
+		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+		"DISPLAY_DATE" => "N",	// Выводить дату элемента
+		"DISPLAY_NAME" => "Y",	// Выводить название элемента
+		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"FILE_404" => "",
+		"FONT_MAX" => "50",
+		"FONT_MIN" => "10",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+		"IBLOCK_ID" => "14",	// Инфоблок
+		"IBLOCK_TYPE" => "news",	// Тип инфоблока
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",	// Включать инфоблок в цепочку навигации
+		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+		"LIST_FIELD_CODE" => "",	// Поля
+		"LIST_PROPERTY_CODE" => "",	// Свойства
+		"MEDIA_PROPERTY" => "",	// Свойство для отображения медиа
+		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+		"META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
+		"META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
+		"NEWS_COUNT" => "10",	// Количество новостей на странице
+		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_TEMPLATE" => "blog",	// Шаблон постраничной навигации
+		"PAGER_TITLE" => "Галерея работ ITProm",	// Название категорий
+		"PERIOD_NEW_TAGS" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+		"SEF_FOLDER" => "/about/gallery/",	// Каталог ЧПУ (относительно корня сайта)
+		"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
+		"SEF_URL_TEMPLATES" => array(
+			"detail" => "#ELEMENT_CODE#/",
+			"news" => "",
+			"section" => "",
+		),
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404
+		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+		"SHOW_404" => "N",	// Показ специальной страницы
+		"SLIDER_PROPERTY" => "",	// Свойство с изображениями для слайдера
+		"SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+		"SORT_BY2" => "NAME",	// Поле для второй сортировки новостей
+		"SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
+		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+		"TAGS_CLOUD_ELEMENTS" => "150",
+		"TAGS_CLOUD_WIDTH" => "100%",
+		"TEMPLATE_THEME" => "blue",	// Цветовая тема
+		"USE_CATEGORIES" => "N",	// Выводить материалы по теме
+		"USE_FILTER" => "N",	// Показывать фильтр
+		"USE_PERMISSIONS" => "N",	// Использовать дополнительное ограничение доступа
+		"USE_RATING" => "N",	// Разрешить голосование
+		"USE_REVIEW" => "N",	// Разрешить отзывы
+		"USE_RSS" => "N",	// Разрешить RSS
+		"USE_SEARCH" => "N",	// Разрешить поиск
+		"USE_SHARE" => "N",	// Отображать панель соц. закладок
+	),
+	false
 );?>
 		</div>
 	</div>
 </div>
- <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- bitrix/templates/webfly_san/components/bitrix/catalog/santech1_5/bitrix/catalog.element/advance/template.php
+++ bitrix/templates/webfly_san/components/bitrix/catalog/santech1_5/bitrix/catalog.element/advance/template.php
@@ -10,6 +10,8 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
+
+
 $this->setFrameMode(true);
 
 $strMainID = $this->GetEditAreaId($arResult['ID']);
